cf277667eaad20cae36cd6e0d73010707751f403
edwinmgallego <ing.edwin.gallego@gmail.com>
2026-09-16 10:17:56 -0500

    mi primer commit

---
+++ src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasegit10am;
+
+/**
+ *
+ * @author emgallego
+ */
+public class ClaseGit10am {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
