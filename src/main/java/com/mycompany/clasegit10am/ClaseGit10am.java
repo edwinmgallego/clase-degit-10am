1c68e92fe954341051b1a79cd37f94aa7a0a9db8
Samuel Lopez <coffeemixer41@outlook.com>
2026-09-16 10:52:20 -0500

    cambios en la rama slopez

--- src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
+++ src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
@@ -12,6 +12,6 @@ public class ClaseGit10am {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World! #2");
+        System.out.println("Hola, soy Samuel Lopez");
     }
 }
