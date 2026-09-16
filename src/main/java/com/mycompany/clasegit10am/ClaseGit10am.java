35e6e9d9f048600571ec537502657ba87edfbc46
samuel.diaz_h <samuel.diaz_h@L137513.uao.edu.co>
2026-09-16 11:08:00 -0500

    samuel diaz herrera cambio 2

--- src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
+++ src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
@@ -14,5 +14,6 @@ public class ClaseGit10am {
         System.out.println("Hello World!");
         System.out.println("Hello World! #2");
         System.out.println("Samuel Diaz Herrera");
+        System.out.println("Samuel Diaz Herrera#2");
     }
 }
