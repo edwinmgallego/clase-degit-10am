67bc696e6ff9abbca494e887975ef44b57c1f6a7
Samuel Lopez <coffeemixer41@outlook.com>
2026-09-16 11:16:08 -0500

    Se elimino el if y se dejo el println

--- src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
+++ src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
@@ -12,13 +12,7 @@ public class ClaseGit10am {
 
     public static void main(String[] args) {
         
-        System.out.println("Hola, soy Samuel Lopez");
-        int numero = 6;
-        if (numero > 5){
-        System.out.println("El numero es mayor a 5!");}
-        else{
-        System.out.println("El numero es menor que 5");}
-             
+        System.out.println("Hola, soy Samuel Lopez");      
     }
          
 }
