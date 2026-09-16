6245dadcc366a7f0f73707fbbc524d854f10afcd
JheffreyUrbano <jheffrey2002@gmail.com>
2026-09-16 11:31:25 -0500

    Actualicé mis archivos con los de los compañeros

--- src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
+++ src/main/java/com/mycompany/clasegit10am/ClaseGit10am.java
@@ -13,9 +13,16 @@ public class ClaseGit10am {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World! #2");
-        System.out.println("Hello World! Jheffrey Urbano en el proyecto del profesor emgallego");
-        System.out.println("cristian Ramirez! #2");
-        System.out.println("cristian Ramirez! #3");
-        System.out.println("Samuel Diaz Herrera");
+        System.out.println("Santiago Sepulveda Perea");
+        System.out.println("Santiago Sepulveda Perea #2");
+        
+        System.out.println("Hola, soy Samuel Lopez");
+        int numero = 6;
+        if (numero > 5){
+        System.out.println("El numero es mayor a 5!");}
+        else{
+        System.out.println("El numero es menor que 5");}
+             
     }
+         
 }
